c070d689122e6d2f7132390ab1c75694f10111c6
nursahtuncel <nursahtuncell@gmail.com>
2024-11-01 19:11:52 +0300

    [OC-4] - tamamlandı

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
-  const navMenu = document.getElementsByClassName("nt-hamburger-menu-2")[0];
+  const navMenu = document.querySelector(".nt-hamburger-menu-2");
   navMenu.classList.toggle("active");
+}
+function toggleSearch() {
+  const searchBox = document.querySelector(".nt-secret-search-box");
+  searchBox.classList.toggle("active");
 }
