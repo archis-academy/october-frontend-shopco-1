d6a754ec2c51c920e80b34c43b0741ef59c27af9
nursahtuncel <nursahtuncell@gmail.com>
2024-11-08 18:49:33 +0300

    [OC-29] - javascript kısmı tamamlandı

--- product-detail.js
+++ product-detail.js
@@ -1,2 +1,28 @@
 // nursahtuncel-OC-29-ımplement the you might also like section start
+
+let priceList = [];
+
+const getNewPrice = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((list) => {
+      priceList = list;
+      createPriceHtml();
+    });
+};
+
+const createPriceHtml = () => {
+  let newPriceEl = document.querySelectorAll(".nt-new-price-old-price-1");
+
+  priceList.forEach((eleman, index) => {
+    if (newPriceEl[index]) {
+      newPriceEl[
+        index
+      ].innerHTML = `<p class="nt-new-price-old-price-1">$${eleman.price}</p>`;
+    }
+  });
+};
+
+getNewPrice();
+
 // nursahtuncel-OC-29-ımplement the you might also like section end
