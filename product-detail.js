57c726ef543fa65e1cc05e9a3ddf765114874971
nursahtuncel <nursahtuncell@gmail.com>
2024-11-12 17:27:17 +0300

    [OC-29] - html yeniden yapıldı js kısmına rating eklendi

--- product-detail.js
+++ product-detail.js
@@ -1,28 +1,57 @@
-// nursahtuncel-OC-29-ımplement the you might also like section start
+// // nursahtuncel-OC-29-ımplement the you might also like section start
 
-let priceList = [];
+let List = [];
 
-const getNewPrice = () => {
+const getProduct = () => {
   fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .then((list) => {
-      priceList = list;
-      createPriceHtml();
+      List = list;
+      getContainer();
     });
 };
+function yildizlar(rating) {
+  let yildizHTML = ``;
 
-const createPriceHtml = () => {
-  let newPriceEl = document.querySelectorAll(".nt-new-price-old-price-1");
+  yildizSayisi = rating - (rating % 1);
+  yarimyildiz = rating % 1;
 
-  priceList.forEach((eleman, index) => {
-    if (newPriceEl[index]) {
-      newPriceEl[
-        index
-      ].innerHTML = `<p class="nt-new-price-old-price-1">$${eleman.price}</p>`;
-    }
+  for (let i = 0; i < yildizSayisi; i++) {
+    yildizHTML += `<img src="images/Star 2.svg" alt="" />`;
+  }
+  if (yarimyildiz >= 0.5) {
+    yildizHTML += `<img src="images/Star 5.png" alt="" />`;
+  }
+  return yildizHTML;
+}
+
+const getContainer = () => {
+  const container = document.querySelector(".nt-images-container");
+  container.innerHTML;
+
+  List.slice(0, 20).forEach((eleman) => {
+    const itemHTML = `
+  
+  <div class="nt-images-container-half-box">
+  
+  <img class="nt-product-image" src="${eleman.image}" alt="${eleman.title}" />
+  <p class="nt-prgrp-bttm">${eleman.title}</p>
+  <div class="nt-stars-and-puan">
+  <div>
+  ${yildizlar(eleman.rating.rate)}
+  </div>
+  <div id="stars">${eleman.rating.rate}}
+  </div>
+  </div>
+  <div class="nt-new-price-old-price">
+  <p class="nt-new-price-old-price-1">$${eleman.price}</p>
+  </div>
+  </div>
+  `;
+    container.innerHTML += itemHTML;
   });
 };
-
-getNewPrice();
+console.log("kodum calısıyo");
+getProduct();
 
 // nursahtuncel-OC-29-ımplement the you might also like section end
