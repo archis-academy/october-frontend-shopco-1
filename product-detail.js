7befdcc1748b251ca9dbc6f065ccd2ee154c5d60
nursahtuncel <nursahtuncell@gmail.com>
2024-11-15 02:00:49 +0300

    [OC-29] - yıldızların rengi değiştirildi .html kısmındaki image yıldızlar kaldırıldı yerine css ile oluşturulmuş yıldızlar eklendi .

--- product-detail.js
+++ product-detail.js
@@ -1,7 +1,5 @@
 // nursahtuncel-OC-29-ımplement the you might also like section start
 
-let List = [];
-
 const getProduct = () => {
   fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
@@ -10,20 +8,19 @@ const getProduct = () => {
       getContainer();
     });
 };
-function yildizlar(rating) {
-  let yildizHTML = ``;
-
-  yildizSayisi = rating - (rating % 1);
-  yarimyildiz = rating % 1;
-
-  for (let i = 0; i < yildizSayisi; i++) {
-    yildizHTML += `<img src="images/Star 2.svg" alt="" />`;
+setRating = (rating) => {
+  let starsHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    if (i <= Math.floor(rating)) {
+      starsHTML += '<div class="star full"></div>';
+    } else if (i <= Math.ceil(rating)) {
+      starsHTML += '<div class="star half"></div>';
+    } else {
+      starsHTML += '<div class="star empty"></div>';
+    }
   }
-  if (yarimyildiz >= 0.5) {
-    yildizHTML += `<img src="images/Star 5.png" alt="" />`;
-  }
-  return yildizHTML;
-}
+  return starsHTML;
+};
 
 const getContainer = () => {
   const container = document.querySelector(".nt-images-container");
@@ -31,25 +28,24 @@ const getContainer = () => {
 
   List.slice(0, 20).forEach((eleman) => {
     const itemHTML = `
-  
-  <div class="nt-images-container-half-box">
-  
-  <div class="nt-image-div">
-  <img class="nt-product-image" src="${eleman.image}" alt="${eleman.title}" />
-  </div>
-  <p class="nt-prgrp-bttm">${eleman.title}</p>
-  <div class="nt-stars-and-puan">
-  <div>
-  ${yildizlar(eleman.rating.rate)}
-  </div>
-  <div id="stars">${eleman.rating.rate}}
-  </div>
-  </div>
-  <div class="nt-new-price-old-price">
-  <p class="nt-new-price-old-price-1">$${eleman.price}</p>
-  </div>
-  </div>
-  `;
+      <div class="nt-images-container-half-box">
+        <div class="nt-image-div">
+          <img class="nt-product-image" src="${eleman.image}" alt="${
+      eleman.title
+    }" />
+        </div>
+        <p class="nt-prgrp-bttm">${eleman.title}</p>
+        <div class="nt-stars-and-puan">
+          <div class="stars">
+            ${setRating(eleman.rating.rate)}
+          </div>
+          <div id="stars">${eleman.rating.rate}</div>
+        </div>
+        <div class="nt-new-price-old-price">
+          <p class="nt-new-price-old-price-1">$${eleman.price}</p>
+        </div>
+      </div>
+    `;
     container.innerHTML += itemHTML;
   });
 };
