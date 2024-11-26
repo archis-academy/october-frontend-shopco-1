f98dacab5810d430c902ef36c98a0385b499034b
Emre <emre.snl52@gmail.com>
2024-11-26 19:10:06 +0300

    [OC-25] - completed

--- product-detail.js
+++ product-detail.js
@@ -54,37 +54,113 @@ const getContainer = () => {
 getProduct();
 
 // nursahtuncel-OC-29-ımplement the you might also like section end
-//emre-OC-25-Product detail hero section start 
+//emre-OC-25-Product detail hero section start
+
 const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get("productId");
+let productId = urlParams.get("productId");
+if (!productId) {
+  productId = 1;
+}
+if (productId > 20) {
+  productId = 20;
+}
 
 function ProductDetails() {
-
   const product = List.find((item) => item.id === parseInt(productId));
 
   document.querySelector(".pd-hs-main-image").src = product.image;
   document.getElementById("small-image1").src = product.image;
   document.getElementById("small-image2").src = product.image;
   document.getElementById("small-image3").src = product.image;
-  // document.querySelector(".pd-hs-main-image").style.opacity=1;
-  document.querySelector(".pd-hs-product-details h1").textContent = product.title;
-  document.querySelector(".pd-hs-description").textContent = product.description;
+  document.querySelector(".pd-hs-product-details h1").textContent =
+    product.title;
+  document.querySelector(".pd-hs-description").textContent =
+    product.description;
 
-  const discountPrice = product.price * 0.60;
-  document.querySelector(".pd-hs-rating-stars").innerHTML=setRating(product.rating.rate);
-  document.querySelector(".pd-hs-rating-number").textContent=product.rating.rate+"/5";
-  document.querySelector(".pd-hs-current-price").textContent =`$${discountPrice.toFixed(0)}` ;
-  document.querySelector(".pd-hs-original-price").textContent = "$"+`${product.price}`;
+  const discountPrice = product.price * 0.6;
+  document.querySelector(".pd-hs-rating-stars").innerHTML = setRating(
+    product.rating.rate
+  );
+  document.querySelector(".pd-hs-rating-number").textContent =
+    product.rating.rate + "/5";
+  document.querySelector(
+    ".pd-hs-current-price"
+  ).textContent = `$${discountPrice.toFixed(0)}`;
+  document.querySelector(".pd-hs-original-price").textContent =
+    "$" + `${product.price}`;
 }
 
 const buttons = document.querySelectorAll(".pd-hs-size-button");
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        buttons.forEach(btn => btn.classList.remove("pd-hs-selected"));
-        button.classList.add("pd-hs-selected");
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("pd-hs-selected"));
+    button.classList.add("pd-hs-selected");
+  });
 });
 
+const buttonColorBrown = document.querySelector(".red");
+const buttonColorGreen = document.querySelector(".green");
+const buttonColorBlue = document.querySelector(".blue");
 
+const mainImage = document.querySelector(".pd-hs-main-image-container");
+const smallImages = document.querySelectorAll(".pd-hs-small-image-container");
 
+buttonColorBrown.addEventListener("click", () => {
+  mainImage.style.backgroundColor = "red";
+
+  smallImages.forEach((image) => {
+    image.style.backgroundColor = "red";
+  });
+});
+
+buttonColorGreen.addEventListener("click", () => {
+  mainImage.style.backgroundColor = "green";
+
+  smallImages.forEach((image) => {
+    image.style.backgroundColor = "green";
+  });
+});
+
+buttonColorBlue.addEventListener("click", () => {
+  mainImage.style.backgroundColor = "blue";
+
+  smallImages.forEach((image) => {
+    image.style.backgroundColor = "blue";
+  });
+});
+
+const minusButton = document.getElementById("pd-hs-minus-btn");
+const plusButton = document.getElementById("pd-hs-plus-btn");
+const quantitySpan = document.querySelector(".quantity-number");
+let quantity = parseInt(quantitySpan.textContent);
+
+minusButton.addEventListener("click", () => {
+  if (quantity > 1) {
+    quantity--;
+    quantitySpan.textContent = quantity;
+  }
+});
+plusButton.addEventListener("click", () => {
+  quantity++;
+  quantitySpan.textContent = quantity;
+});
+const smallImagesSrc = document.querySelectorAll(".pd-hs-small-image");
+const mainImageSrc = document.querySelector(".pd-hs-main-image");
+smallImagesSrc.forEach((image) => {
+  image.addEventListener("click", () => {
+    mainImageSrc.src = image.src;
+    smallImagesSrc.forEach((img) => {
+      img.parentElement.classList.remove("pd-hs-selected-image-active");
+    });
+    image.parentElement.classList.add("pd-hs-selected-image-active");
+  });
+});
+
+const colorbuttons = document.querySelectorAll('.pd-hs-color');
+colorbuttons.forEach(button => {
+    button.addEventListener('click', () => {
+        colorbuttons.forEach(btn => btn.classList.remove('pd-hs-color-active'));
+        button.classList.add('pd-hs-color-active');
+    });
+});
 //emre-OC-25-Product detail hero section end
