27ba18bd16cb52ea7d7a81bb3fe157a8d597ca2d
melikeaksoy555 <melikeficici@hotmail.com>
2024-11-29 19:07:02 +0300

    [OC-14] - ilaveler yapıldı

--- app.js
+++ app.js
@@ -9,3 +9,64 @@ function toggleSearch() {
   searchBox.classList.toggle("active");
 }
 // nt-header-end............................
+
+// Create-the-Top-Selling-section start
+
+let List = [];
+const getProduct = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((list) => {
+      List = list;
+      getContainer();
+    });
+};
+setRating = (rating) => {
+  let starsHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    if (i <= Math.floor(rating)) {
+      starsHTML += '<div class="star full"></div>';
+    } else if (i <= Math.ceil(rating)) {
+      starsHTML += '<div class="star half"></div>';
+    } else {
+      starsHTML += '<div class="star empty"></div>';
+    }
+  }
+  return starsHTML;
+};
+
+const getContainer = () => {
+  const container = document.querySelector(".products");
+  container.innerHTML = "";
+
+  List.slice(0, 4).forEach((eleman) => {
+    const discountPrice = eleman.price * 0.6;
+
+    const itemHTML = `
+<div class="product">
+<div class="image-container">
+              <img class="top-selling-image"
+                src="${eleman.image}"
+                alt="${eleman.title}"
+              />
+              </div> 
+              <h2>${eleman.title}</h2>
+              <div class="nt-stars-and-puan">
+          <div class="stars">
+            ${setRating(eleman.rating.rate)} 
+            <div id="point-stars">${eleman.rating.rate}/5</div>
+          </div>   
+        </div>
+        <div class="price">
+        <p class="current-price">$${discountPrice.toFixed(2)}</p>
+              <p class="original-price">$${eleman.price}</p>
+              <button class= "discount">40%</button>
+        </div>
+            </div>
+    `;
+    container.innerHTML += itemHTML;
+  });
+};
+getProduct();
+
+// Create-the-Top-Selling-section end
