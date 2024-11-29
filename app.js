2eb31845b49f5cd887802d590586fdca29500ec9
Betul351 <betulyilmaz1651@gmail.com>
2024-11-29 19:02:54 +0300

    [OC-18] - Add JSON data for testimonials

--- app.js
+++ app.js
@@ -10,7 +10,43 @@ function toggleSearch() {
 }
 // nt-header-end............................
 
-//ctt-slider-btn______________Start
+//ctt-slider______________Start
+//
+document.addEventListener("DOMContentLoaded", function(){
+  fetch('./constants/yorumlar.json')
+  .then(response => response.json())
+  .then(testimonials => {
+    const sliderContainer = document.querySelector('.slider');
+
+    testimonials.forEach(testimonial => {
+      const slide = document.createElement('div');
+      slide.classList.add('slide');
+
+      const starRate = document.createElement('div');
+      starRate.classList.add('star-rate');
+
+      const user = document.createElement('div');
+      user.classList.add('user');
+      const userName = document.createElement('h4');
+      userName.innerText = testimonial.user;
+      const userImage = document.createElement('img');
+      userImage.src = './images/Create the Testimonial/approved user.svg';
+      user.appendChild(userName);
+      user.appendChild(userImage);
+
+      const review = document.createElement('div');
+      review.classList.add('review');
+      review.innerHTML = `<p>${testimonial.review}</p>`;
+
+      slide.appendChild(starRate);
+      slide.appendChild(user);
+      slide.appendChild(review);
+
+      sliderContainer.appendChild(slide);
+    });
+
+
+    //Button of slider
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
@@ -30,6 +66,18 @@ prev.addEventListener('click', () => {
   console.log(slider.scrollLeft)
 })
 
+.catch(error => {  
+  console.error('Error code: OC-18', error);
+});
 
 
-//ctt-slider-btn______________End
+
+  })
+  
+});
+
+
+
+
+
+//ctt-slider______________End
