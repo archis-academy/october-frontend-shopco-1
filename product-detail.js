777b0854424eaeea685809a6ae5d247ee8f4e350
nursahtuncel <nursahtuncell@gmail.com>
2024-12-02 17:21:58 +0300

    [OC-28] - js kodları yazıldı

--- product-detail.js
+++ product-detail.js
@@ -12,7 +12,7 @@ tabItems.forEach((tab, index) => {
     tabPanes[index].classList.add("active");
   });
 });
-// **************************Dropdown menü aç/kapat**********************
+//*************
 const dropdownToggle = document.querySelector(".dropdown-toggle");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
@@ -20,76 +20,72 @@ dropdownToggle.addEventListener("click", () => {
   dropdownMenu.style.display =
     dropdownMenu.style.display === "block" ? "none" : "block";
 });
-
-// ****************Dropdown menüdeki bir seçenek seçildiğinde***********
+//***********
 dropdownMenu.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
     dropdownToggle.textContent = e.target.textContent;
     dropdownMenu.style.display = "none";
   }
 });
-
-// *********************"Write a Review" butonu***************
+//************
 document.querySelector(".write-review-btn").addEventListener("click", () => {
   alert("bu kısmı daha sonra ekleyeceğim");
 });
 
-// **************************review******************
-fetch("content.json")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Veri alınamadı: " + response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const reviewContainer = document.querySelector(".review-container");
-    reviewContainer.innerHTML = `
+// ***********
+
+let reviewsArray = [];
+
+const getReviews = () => {
+  fetch(".vscode/content.json")
+    .then((response) => response.json())
+    .then((reviews) => {
+      reviewsArray = reviews;
+      getAllReviews();
+    });
+};
+
+const getAllReviews = () => {
+  const reviewContainer = document.querySelector(".review-container");
+  reviewContainer.innerHTML = "";
+
+  reviewsArray.slice(0, 6).forEach((data) => {
+    const dataHTML = `
  <div class="reviews-nt">
-    <div class="author-commert-date-container">
- <div class="rating-review-container">
-   <div>
-   <img src="images/review-section/rating.svg" alt="">
+  <div class="author-commert-date-container">
+  <div class="rating-review-container">
+  <div>
+<img src="images/review-section/rating.svg" alt="">
+  </div>
+   <div class ="ellipsis-nt">
+    <img src="images/review-section/ücnokta.svg" alt="">
+  </div>
+</div>
+<div class="author-review-container">
+  <p>${data.ad} ${data.soyad}</p>
+</div>
+<div class="comment-review-container">
+  <p>${data.yorum}</p>
+</div>
    </div>
-      <div> <img src="images/review-section/ücnokta.svg" alt=""></div>
- </div>
-  
-      <div class="author-review-container">
- <p> ${data.ad}</p>
-      </div>
-      <div class="comment-review-container">
- <p>
- ${data.yorum}
- </p>
-      </div>
-    </div>
-      <div class="date-review-nt">
- <p>Posted on ${data.tarih}</p>     </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <p>İsim: ${data.name}</p>
-      <p>Yaş: ${data.age}</p>
-      <p>Şehir: ${data.city}</p>
-    `;
-  })
-  .catch((error) => {
-    console.error("Bir hata oluştu:", error);
+   <div class="date-review-nt">
+<p>Posted on ${data.tarih}</p>
+   </div>
+ </div>`;
+    reviewContainer.innerHTML += dataHTML;
   });
+};
 
+getReviews();
 // nursahtuncel-OC-28-review section end
+//  soyadının yalnızca ilk harfi alınacak
+// yıldızları da json dosyasından al
+// elipsis için bir özellik gerekiyor mu??
+//Satoshi yazı fontunu import et.
+//butona click özelliği ekle.
+//dropdown menude tıklanılan bölüme göre yorumları filtrele
+//yorum yaz kısmı için bir bölüm oluştur.
+
 // nursahtuncel-OC-29-ımplement the you might also like section start
 let List = [];
 const getProduct = () => {
@@ -124,25 +120,24 @@ const getContainer = () => {
   List.slice(0, 20).forEach((eleman) => {
     const discount = eleman.price * 0.6;
     const itemHTML = `
-      <div class="nt-images-container-half-box">
-        <div onclick="navigateDetail(${eleman.id})" class="nt-image-div">
-          <img class="nt-product-image" src="${eleman.image}" alt="${
-      eleman.title
-    }" />
-        </div>
-        <p class="nt-prgrp-bttm">${eleman.title}</p>
-        <div class="nt-stars-and-puan">
-          <div class="stars">
-            ${setRating(eleman.rating.rate)}
-          </div>
-          <div id="stars">${eleman.rating.rate}</div>
-        </div>
-        <div class="nt-new-price-old-price">
-          <p class="nt-new-price-old-price-1">$${discount.toFixed(2)}</p>
-             <p class="nt-new-price-old-price-2">$${eleman.price}</p>
-          <p class="nt-new-price-old-price-3">-40%</p>
-        </div>
-      </div>
+ <div class="nt-images-container-half-box">
+   <div onclick="navigateDetail(${eleman.id})" class="nt-image-div">
+<img class="nt-product-image" src="${eleman.image}" alt="${eleman.title}" />
+   </div>
+   <p class="nt-prgrp-bttm">${eleman.title}</p>
+   <div class="nt-stars-and-puan">
+<div class="stars">
+  ${setRating(eleman.rating.rate)}
+</div>
+<div id="stars">${eleman.rating.rate}</div>
+   </div>
+   <div class="nt-new-price-old-price">
+   <p class="nt-new-price-old-price-1">$${discount.toFixed(2)}</p>
+    <p class="nt-new-price-old-price-2">$${eleman.price}</p>
+   <p class="nt-new-price-old-price-3">-40%</p>
+   </div>
+   </div>
+
     `;
     container.innerHTML += itemHTML;
   });
